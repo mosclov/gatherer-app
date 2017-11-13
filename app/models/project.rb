ec5659d1e5e1033966a0fc876d69d20a7e9b5b43
mosclov <federico@grupofemal.com>
2017-11-12 23:30:00 -0800

    first commit, project rspec

---
+++ app/models/project.rb
@@ -0,0 +1,23 @@
+class Project < ActiveRecord::Base
+  attr_accessor :tasks
+
+  def initialize
+    @tasks = []
+  end
+
+  def incomplete_tasks
+    tasks.reject(&:complete?)
+  end
+
+  def done?
+    incomplete_tasks.empty?
+  end
+
+  def total_size
+    tasks.sum(&:size)
+  end
+
+  def remaining_size
+    incomplete_tasks.sum(&:size)
+  end
+end
---
+++ app/models/task.rb
@@ -0,0 +1,25 @@
+class Task < ActiveRecord::Base
+  attr_accessor :size, :completed_at
+
+  def initialize(options = {})
+    mark_completed(options[:completed]) if options[:completed_at]
+    @size = options[:size]
+  end
+
+  def mark_completed(date = nil)
+    @completed_at = (date || Time.current)
+  end
+
+  def complete?
+    completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > 3.weeks.ago
+  end
+
+  def points_toward_velocity
+    part_of_velocity? ? size : 0
+  end
+end
